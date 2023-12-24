411414b082ac5a28a1231466c3bf4c81209571a2
alistairyes <141379399+alistairyes@users.noreply.github.com>
2023-12-24 00:25:44 -0500

    Update and rename script.js to EventList.vue

---
+++ EventList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.title }} - {{ event.startDate }} to {{ event.endDate }}
+      </li>
+    </ul>
+    <button @click="generateICS">Generate ICS File</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['events'],
+  methods: {
+    generateICS() {
+      // Implement logic to generate ICS file from events
+    }
+  }
+};
+</script>
