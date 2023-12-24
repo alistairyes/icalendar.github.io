f3ce017e5535e1c8425c7f129d0265bfcf1e1082
alistairyes <141379399+alistairyes@users.noreply.github.com>
2023-12-24 00:53:12 -0500

    Update EventList.vue

--- EventList.vue
+++ EventList.vue
@@ -5,7 +5,7 @@
         {{ event.title }} - {{ event.startDate }} to {{ event.endDate }}
       </li>
     </ul>
-    <button @click="generateICS">Generate ICS File</button>
+    <button @click="generateICS(events)">Generate ICS File</button>
   </div>
 </template>
 
@@ -13,8 +13,26 @@
 export default {
   props: ['events'],
   methods: {
-    generateICS() {
-      // Implement logic to generate ICS file from events
+    generateICS(events) {
+      let icsContent = 'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Your App//EN\n';
+      events.forEach(event => {
+        icsContent += 'BEGIN:VEVENT\n';
+        icsContent += `SUMMARY:${event.title}\n`;
+        icsContent += `DTSTART:${this.formatDateToICS(event.startDate, event.startTime)}\n`;
+        icsContent += `DTEND:${this.formatDateToICS(event.endDate, event.endTime)}\n`;
+        icsContent += 'RRULE:FREQ=WEEKLY\n'; // Add logic for repetition
+        icsContent += 'END:VEVENT\n';
+      });
+      icsContent += 'END:VCALENDAR';
+
+      const blob = new Blob([icsContent], { type: 'text/calendar' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'events.ics';
+      link.click();
+    },
+    formatDateToICS(date, time) {
+      // Convert date and time to the ICS format
     }
   }
 };
