2c035b65249cbf2fb372645b0f3a636d0af627d2
alistairyes <141379399+alistairyes@users.noreply.github.com>
2023-12-24 00:27:03 -0500

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <EventForm @new-event="addEvent" />
+    <EventList :events="events" />
+  </div>
+</template>
+
+<script>
+import EventForm from './EventForm.vue';
+import EventList from './EventList.vue';
+
+export default {
+  components: {
+    EventForm,
+    EventList
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  methods: {
+    addEvent(event) {
+      this.events.push({...event, id: Date.now() });
+    }
+  }
+};
+</script>
