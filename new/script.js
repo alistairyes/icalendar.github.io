8ce65fffbf1766e63ec3c8731868c1195eda60b5
alistairyes <141379399+alistairyes@users.noreply.github.com>
2023-12-24 10:10:53 -0500

    Update script.js

--- new/script.js
+++ new/script.js
@@ -27,15 +27,79 @@ async function populateDropdown() {
     }
 }
 
-// Function to handle course selection
+// Function to handle course selection and removal from dropdown
 function handleCourseSelection(event) {
-    // Logic to handle course selection and removal from dropdown
+    const selectedCourse = event.target.value; // Get the selected course name
+
+    // Check if the selected course has already been chosen
+    const selectedCoursesList = document.getElementById('selected-courses-list');
+    const selectedCourseItems = selectedCoursesList.getElementsByTagName('li');
+
+    for (const item of selectedCourseItems) {
+        if (item.textContent === selectedCourse) {
+            alert('You have already selected this course.'); // Display a message or take another action if desired
+            event.target.value = ''; // Clear the dropdown selection
+            return; // Exit the function if the course is already selected
+        }
+    }
+
+    if (selectedCourse !== '') {
+        // Create a new list item to display the selected course
+        const listItem = document.createElement('li');
+        listItem.textContent = selectedCourse;
+
+        // Create a button to delete the selected course
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'btn btn-danger btn-sm ml-2';
+
+        // Add an event listener to the delete button
+        deleteButton.addEventListener('click', () => {
+            handleCourseDeletion(selectedCourse);
+        });
+
+        // Append the delete button to the list item
+        listItem.appendChild(deleteButton);
+
+        // Append the list item to the selected courses list
+        selectedCoursesList.appendChild(listItem);
+
+        // Clear the dropdown selection to prevent duplication
+        event.target.value = '';
+    }
 }
+
+// ... Rest of your code ...
+
 
 // Function to handle course deletion
 function handleCourseDeletion(courseName) {
-    // Logic to handle course deletion and adding it back to the dropdown
+    // Remove the course from the selected courses list
+    const selectedCoursesList = document.getElementById('selected-courses-list');
+    const selectedCourseItems = selectedCoursesList.getElementsByTagName('li');
+
+    let courseFound = false; // Flag to track if the course is found
+
+    for (const item of selectedCourseItems) {
+        if (item.textContent === courseName) {
+            selectedCoursesList.removeChild(item);
+            courseFound = true; // Set the flag to true if the course is found
+            break; // Exit the loop once the course is removed
+        }
+    }
+
+    if (!courseFound) {
+        alert(`Course "${courseName}" not found in the selected courses list.`);
+    }
+
+    // Add the deleted course back to the dropdown
+    const dropdown = document.getElementById('course-dropdown');
+    const option = document.createElement('option');
+    option.value = courseName;
+    option.textContent = courseName;
+    dropdown.appendChild(option);
 }
+
 
 // Event listener to populate the dropdown on page load
 window.addEventListener('load', populateDropdown);
@@ -43,7 +107,22 @@ window.addEventListener('load', populateDropdown);
 // Event listener to handle course selection
 document.getElementById('course-dropdown').addEventListener('change', handleCourseSelection);
 
-// Event listener to handle course deletion (you can implement this as a button click event)
+// Event listener to handle course deletion (implement as a button click event)
+document.getElementById('delete-button').addEventListener('click', () => {
+    const selectedCoursesList = document.getElementById('selected-courses-list');
+    const selectedCourseItems = selectedCoursesList.getElementsByTagName('li');
+
+    if (selectedCourseItems.length === 0) {
+        alert('No selected courses to delete.'); // Display a message if no courses are selected
+    } else {
+        // Prompt the user to select a course to delete
+        const courseToDelete = prompt('Enter the name of the course to delete:');
+
+        if (courseToDelete) {
+            handleCourseDeletion(courseToDelete.trim()); // Trim to remove leading/trailing spaces
+        }
+    }
+});
 // Example: document.getElementById('delete-button').addEventListener('click', handleCourseDeletion);
 
 
